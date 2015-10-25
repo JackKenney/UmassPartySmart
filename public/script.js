abe21010220bc1ce98e184c620e29accc1fb65f2
Jack Kenney <jnormandkenney@gmail.com>
2015-10-25 09:08:34 -0400

    latest almost done

--- public/script.js
+++ public/script.js
@@ -22,12 +22,13 @@ socket.on('query-response', function( reply ) {
 // MAIN ROUTINE, RUNS ON DOCUMENT LOAD
 
 $(document).ready( function () {
+  //Global Vars
+  var delivery_manifest;
   //FIELDS:
   var firstName = $(document.getElementById('first_name')),
       lastName = $(document.getElementById('last_name')),
       student = $(document.getElementById('student_id')),
       phone = $(document.getElementById('phone_number')),
-      phone2 = $(document.getElementById('phone_number2')),
       provider = $(document.getElementById('provider_options')),
       address = $(document.getElementById('address')),
       address2 = $(document.getElementById('address2')),
@@ -49,7 +50,6 @@ $(document).ready( function () {
           'last_name':lastName.val().toLowerCase(),
           'student_id':student.val().toLowerCase(),
           'phone':phone.val().toLowerCase(),
-          'phone2':phone2.val().toLowerCase(),
           'provider':provider.val().toLowerCase(),
           'address':address.val().toLowerCase(),
           'address2':address2.val().toLowerCase(),
@@ -69,6 +69,7 @@ $(document).ready( function () {
     conf.css('display','block');
   });
 
+  //BEGIN Police Page Interactions
   //When Police Address Form submission is clicked:
   // p for police
   var pAddress = $(document.getElementById('paddress')),
@@ -95,14 +96,14 @@ $(document).ready( function () {
     socket.emit('police-query', data);
     console.log("Police Query Submitted");
   });
-  socket.on('police-response', function(data) { // { first_name:"sally", phone:6, phone2:2345 }
+  socket.on('police-response', function(data) { // { first_name:"sally", phone:6}
     console.log(data);
     data = data.rows;
     var table = $(document.createElement('table')),
         tblBody=$(document.createElement('tbody'));
     for(var r=-1;r<data.length;r++) {
       var row = document.createElement('tr');
-      for(var c=0;c<3;c++) {
+      for(var c=0;c<2;c++) {
         var cell = document.createElement('td'),
             cellText;
         switch (c) {
@@ -114,10 +115,6 @@ $(document).ready( function () {
             if(r<0)  cellText = document.createTextNode("Phone Number");
             else cellText = document.createTextNode(data[r].phone);
             break;
-          case 2:
-            if(r<0)  cellText = document.createTextNode("Additional Phone");
-            else cellText = document.createTextNode(data[r].phone2);
-            break;
         }
         cell.appendChild(cellText);
         row.appendChild(cell);
@@ -128,6 +125,189 @@ $(document).ready( function () {
     $(document.getElementById('returnTable')).append(table);
 
   });
+  //End Police Page Interactions
+
+  //Complaint Form Interactions BEGIN
+    //c for complaint form
+  var cAddress = $(document.getElementById('yaddress')),
+      cAddress2 = $(document.getElementById('yaddress2')),
+      cCity = $(document.getElementById('ycity')),
+      cState = $(document.getElementById('ystate')),
+      cZip = $(document.getElementById('yzipcode')),
+
+      partyAdd = $(document.getElementById('partyaddress')),
+      partyAdd2 = $(document.getElementById('partyaddress2')),
+      partyCity = $(document.getElementById('partycity')),
+      partyState = $(document.getElementById('partystate')),
+      partyZip = $(document.getElementById('partyzipcode')),
+
+      cSubmit = $(document.getElementById('ysubmit'));
+  
+  cSubmit.on('click', function() {
+    var cAdd = 
+          cAddress.val() +", "+
+          cAddress2.val() +", "+
+          cCity.val() +", "+
+          cState.val() +", "+
+          cZip.val(),
+        pAdd =
+          partyAdd.val() +", "+
+          partyAdd2.val() +", "+
+          partyCity.val() +", "+
+          partyState.val() +", "+
+          partyZip.val(),
+        distance;
+    console.log(cAdd + pAdd);
+    if(cAdd !=", , , , " && pAdd!=", , , , ") {
+      getCoordinates(cAdd, function(cCoordinates) { 
+        getCoordinates(pAdd, function(pCoordinates) { 
+          distance = getDistanceMiles(
+            cCoordinates[0],
+            cCoordinates[1],
+            pCoordinates[0],
+            pCoordinates[1]
+          ); 
+          if(distance <= .25) { 
+            data = {
+              'address':partyAdd.val().toLowerCase(),
+              'address2':partyAdd2.val().toLowerCase(),
+              'city':partyCity.val().toLowerCase(),
+              'state':partyState.val().toLowerCase(),
+              'zip':partyZip.val()
+            };
+            socket.emit('text-info-query',data);
+          }
+        });
+      });
+    } else if(pAdd!=", , , , " && navigator.geolocation) {
+      //u for user
+      var uLat, uLng;
+      navigator.geolocation.getCurrentPosition(function(position) {
+        uLat = position.coords.latitude;
+        uLng = position.coords.longitude;
+
+        getCoordinates(pAdd, function(pCoords) {
+          distance = getDistanceMiles(
+            uLat,
+            uLong,
+            pCoords[0],
+            pCoords[1] 
+          );
+          if(distance <= .25) {
+            data = {
+              'address':partyAdd.val().toLowerCase(),
+              'address2':partyAdd2.val().toLowerCase(),
+              'city':partyCity.val().toLowerCase(),
+              'state':partyState.val().toLowerCase(),
+              'zip':partyZip.val()
+            };
+            socket.emit('text-info-query',data);
+          }
+        });
+      });
+    } else {
+      $('.error').css('display','block'); 
+    }
+  });
+
+  //Distance Calculations and Location:
+  var geocoder = new google.maps.Geocoder();
+
+  function getCoordinates( address, callback) {
+    var coordinates;
+    geocoder.geocode({ 'address': address}, function (results, status) {
+      var coords_obj = results[0].geometry.viewport;
+      console.log("1: "  + coords_obj);
+      console.log("test: " + coords_obj.j.j);
+      coordinates = [coords_obj.O.O, coords_obj.j.j];
+        callback(coordinates);
+    });
+  }
+  function getDistanceMiles(lat1,lon1,lat2,lon2) {
+    var R = 0.621371 * 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; //* 0.621371; // Distance in miles
+    return d;
+  }
+  function deg2rad(deg) {
+    return deg * (Math.PI/180);
+  }
+//Delivery List Page
+var display = $(document.getElementById('displayButton')),
+    tableDiv = $(document.getElementById('tableDiv')),
+    edit = $(document.getElementById('editButton'));
+
+display.on('click', function() {
+  socket.emit('delivery-query');
+});
+edit.on('click', function() {
+
+});
+socket.on('delivery-report', function(data) {
+    delivery_manifest = data
+
+    var table = $(document.createElement('table')),
+        tblBody=$(document.createElement('tbody'));
+    for(var r=-1;r<data.length;r++) {
+      var row = document.createElement('tr');
+      for(var c=0;c<7;c++) {
+        var cell = document.createElement('td'),
+            cellText;
+        switch (c) {
+          case 0:
+            if(r<0)  cellText = document.createTextNode("Name");
+            else cellText = document.createTextNode(data[r].first_name+" ");
+            break;
+          case 1:
+            cellText = document.createTextNode(data[r].last_name);
+            break;
+          case 2:
+            if(r<0)  cellText = document.createTextNode("Phone Number");
+            else cellText = document.createTextNode(data[r].phone);
+            break;
+          case 3:
+            if(r<0)  cellText = document.createTextNode("Address");
+            else cellText = document.createTextNode(data[r].address +", "+
+                data[r].address2 +", "+
+                data[r].city +", "+
+                data[r].state +", "+
+                data[r].zipcode
+              );
+            break;
+          case 4:
+            if(r<0)  cellText = document.createTextNode("Date");
+            else cellText = document.createTextNode(data[r].date);
+            break;
+          case 5:
+            if(r<0)  cellText = document.createTextNode("Time");
+            else cellText = document.createTextNode(data[r].time);
+            break;
+          case 6:
+            if(r<0)  cellText = document.createTextNode("Status");
+            else {
+              cellText = document.createElement('input');
+              if(data[r].delivery_status>0) cellText.addAttribute("checked disabled");
+              else cellText.addClass("unchecked");
+            }
+            break;
+        }
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      }
+      tblBody.append(row);
+    }
+    table.append(tblBody);
+    $(document.getElementById('returnTable')).append(table);
+  
+});
+
+
 
 //SOCKET INTERACTIONS
   //how client deals with Response Data
--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var express = require('express'),
     io = require('socket.io')(http),
 //database
     mysql = require( 'mysql' ),
-    connection = mysql.createConnection('mysql://root:password@localhost/Parties');
+    connection = mysql.createConnection('mysql://root:password@localhost/Parties'),
+//email client
+    mandrill = require('node-mandrill')('nq8e1lZhVI_LMO-0hy5mog');
 //server listens on port 1337
 http.listen(1337);
 
@@ -18,16 +20,18 @@ console.log('Database Connection Established!');
 
 io.on('connection', function(socket) {
   console.log('Connection to Client Established!');
-  var date = new Date().getTime(),
-      check,
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth(),
+      day = dateObj.getUTCDate(),
+      year = dateObj.getUTCFullYear();
+  var today = new Date(year,month,day),
       sql = "SELECT party_id, date FROM Parties";
   connection.query(sql, function(err, rows, fields) {
     if(err) console.log(err);
     else {
       for(var o in rows) {
-        check = new Date(rows[o].date.toString());
-        if(check < date) {
-          console.log(rows[o].party_id)
+        check = new Date(rows[o].date);
+        if(check < today) {
           connection.query("DELETE FROM Parties WHERE party_id=" + rows[o].party_id);
         }
       }
@@ -44,18 +48,17 @@ io.on('connection', function(socket) {
       if(err) console.log(err);
       else provID = rows[0].provider_id;
       console.log("2 "+provID);
-      sql = "INSERT INTO Parties (student_id, first_name, last_name, phone, phone2, provider, address, address2, city, state, zipcode, date, time) VALUES(";
+      sql = "INSERT INTO Parties (student_id, first_name, last_name, phone, provider_id, address, address2, city, state, zipcode, date, time) VALUES(";
       sql += "\"" +data.student_id+ "\",";
       sql += "\"" +data.first_name+ "\",";
       sql += "\"" +data.last_name+ "\",";
       sql += "\"" +data.phone+ "\",";
-      sql += "\"" +data.phone2+ "\",";
       sql += "\"" +provID+ "\",";
       sql += "\"" +data.address+ "\",";
       sql += "\"" +data.address2+ "\",";
       sql += "\"" +data.city+ "\",";
-      sql += "\"" +data.zip+ "\",";
       sql += "\"" +data.state+ "\",";
+      sql += "\"" +data.zip+ "\",";
       sql += "\"" +data.date+ "\",";
       sql += "\"" +data.time+ "\");";
       connection.query(sql, function(err,rows,fields) {
@@ -71,51 +74,110 @@ io.on('connection', function(socket) {
   //when a new post is submitted, the sql is sent to the database through here and errors are returned.
   socket.on('police-query', function(data) { //{address,address2,city,state,zip}
     console.log('New Police Query Received!');
-
-    //Returns all rows associated with that address.
-    var sql = "SELECT first_name, phone FROM Parties WHERE ",
-        result;
-    
-    //Address Line 1
-    if(data.address!="") {
-        sql += "address LIKE \'%" + data.address + "%\'";
-        sql += " OR ";
-    }
-    //Address Line 2
-    if(data.address2!="") {
-        sql += "address2 LIKE \'%" + data.address2 + "%\'";
-        sql += " OR ";
-    }
-    //City Line
-    if(data.city!="") {
-        sql += "city LIKE \'%" + data.city + "%\'";
-        sql += " OR "; 
-    }
-    //State Line
-    if(data.state!="") {
-        sql += "state LIKE \'%" + data.state + "%\'";
-        sql += " OR ";
-    }
-    //Zipcode line
-    if(data.zip!="") {
-        sql += "zipcode LIKE \'%" + data.zip + "%\'";
-        sql += " OR ";
-    }
-    sql = sql.substring(0,sql.length-4) + ';';
-    console.log(sql);
+    var sql_start =  "SELECT first_name, phone, date FROM Parties WHERE ";
+    populateSearch(sql_start, data, function(sql) {
     //Query Database to return party info
-    connection.query(sql, function(err,rows,fields) {
-      if(err) console.log(err);
-      else {  
-        result = { 'rows':rows };
-        console.log(result);
-        io.emit('police-response', result);
-      }
+      connection.query(sql, function(err,rows,fields) {
+        if(err) console.log(err);
+        else {
+          var result = [],
+              rowCount = 0;  
+          for(var i=0;i<rows.length;i++) { //remove all parties that are not happening today
+            if(today.getTime() == rows[i].date.getTime()) {
+              result[rowCount] = rows[i];
+              rowCount++;
+            }
+          }
+          var result = { rows:result };
+          console.log(result);
+          io.emit('police-response', result);
+        }
+      });
     });
     console.log("Police Response Sent!"); 
   });
+  //End Police Sockets
 
-});
+  //Start Complaint Sockets
+  socket.on('text-info-query', function(data) { // {address,address2,city,state,zip}
+    console.log('Text-info Recieved!');
+    var sql_start = "SELECT Providers.extension, Parties.phone, Parties.date FROM Parties JOIN Providers ON Parties.provider_id = Providers.provider_id WHERE ";
+    populateSearch(sql_start, data, function(sql) {
+      console.log(sql);
+      connection.query(sql, function(err,rows,fields) {
+        if(err) console.log(err);
+        else { // (hopefully) rows is [ { extension,phone } ]
+          for(var i=0; i<rows.length; i++) {
+            var obj = rows[i];
+              sendText(obj.phone,obj.extension,"","Hi, this is your courtesy text. Your party is too loud, please tone it down because your neighbors are ready to contact the authorities.");
+          }
+        }
+      }); 
+    });
+  });
+  
+  //Begin Delivery Interactions:
+  socket.on('delivery-query', function() {
+    var sql = "SELECT first_name, last_name, phone, address, address2, city, state, zipcode, date, time, delivery_status FROM Parties WHERE date<" + (today.getTime()+604800000);
+    connection.query(sql, function(err,rows,fields) {
+      if(err) console.log(sql);
+      else {
+        console.log(rows);
+      }
+    });
+  });
+}); //END OF SOCKETS
+//Adjunct function to Send text to given number through provider's email client
+function sendText (_number, _extension, _subject, _message ) {
+  var email = "";
+  email += _number + _extension;
+  mandrill('/messages/send', {
+    message: {
+        to: [{'email':email},{name:'Party Host'}],
+        from_email: 'noreply@partysmart.com',
+        subject:"UMass Party Smart",
+        text:_message
+    }    
+  }, function(error, response) {
+    if(error) console.log(error);
+    else console.log(response);
+  });
+}
+//End Complaint Sockets
+
+
+//Adjunct function, abstracted for efficiency, to provide sql database search WHERE clauses from JSON files
+function populateSearch(sql_start, data, callback) { // {address,address2,city,state,zip}
+  var sql = sql_start;
+  //Address Line 1
+  if(data.address!="") {
+    sql += "address LIKE \'%" + data.address + "%\'";
+    sql += " AND ";
+  }
+  //Address Line 2
+  if(data.address2!="") {
+    sql += "address2 LIKE \'%" + data.address2 + "%\'";
+    sql += " AND ";
+  }
+  //City Line
+  if(data.city!="") {
+    sql += "city LIKE \'%" + data.city + "%\'";
+    sql += " AND ";
+  }
+  //State Line
+  if(data.state!="") {
+    sql += "state LIKE \'%" + data.state + "%\'";
+    sql += " AND ";
+  }
+  //Zipcode line
+  if(data.zip!="") {
+    sql += "zipcode LIKE \'%" + data.zip + "%\'";
+    sql += " AND ";
+  }
+  sql = sql.substring(0,sql.length-4) + ';';
+  callback(sql);
+}
+
 //send page to address on req. to default directory
   app.use(express.static(__dirname + '/public'));
 
