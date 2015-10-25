1df6a335c0e9b8e58381bcc8e17216f66b9f7c83
Jack Kenney <jnormandkenney@gmail.com>
2015-10-24 22:02:19 -0400

    lastest commit

--- public/script.js
+++ public/script.js
@@ -23,23 +23,25 @@ socket.on('query-response', function( reply ) {
 
 $(document).ready( function () {
   //FIELDS:
-  var firstNameIn = $(document.getElementById('first_name'),
-      lastNameIn = $(document.getElementById('last_name'),
-      studentIn = $(document.getElementById('student_id'),
-      phoneIn = $(document.getElementById('phone_number'),
-      phoneIn2 = $(document.getElementById('phone_number2'),
-      addressIn = $(document.getElementById('address'),
-      address2In = $(document.getElementById('address2'),
-      cityIn = $(document.getElementById('city'),
-      stateIn = $(document.getElementById('state'),
-      zipIn = $(document.getElementById('zipcode'),
-      dateIn = $(document.getElementById('date'),
-      timeIn = $(document.getElementById('time'),
+  var firstNameIn = $(document.getElementById('first_name')),
+      lastNameIn = $(document.getElementById('last_name')),
+      studentIn = $(document.getElementById('student_id')),
+      phoneIn = $(document.getElementById('phone_number')),
+      phoneIn2 = $(document.getElementById('phone_number2')),
+      addressIn = $(document.getElementById('address')),
+      address2In = $(document.getElementById('address2')),
+      cityIn = $(document.getElementById('city')),
+      stateIn = $(document.getElementById('state')),
+      zipIn = $(document.getElementById('zipcode')),
+      dateIn = $(document.getElementById('date')),
+      timeIn = $(document.getElementById('time')),
 
-      submitButton = $(document.getElementById('submit');
+      submitButton = $(document.getElementById('submit')),
+      
+      conf = $(document.getElementById('conf')); //registration confirmation
 
-  //if display button is clicked, fetch table data and build table
-  submitButton.on('click', function() { //first_name, last_name, student_id, phone, address, date, time 
+  //When Registration Submit Button is Clicked:
+  submitButton.on('click', function() {  
     console.log('Submit Button Clicked');
     var data = { 
           'first_name':firstNameIn.val(),
@@ -47,20 +49,64 @@ $(document).ready( function () {
           'student_id':studentIn.val(),
           'phone':phoneIn.val(),
           'phone2':phoneIn2.val(),
-          'address':addressIn.val(),
-          'address2':address2In.val(),
-          'city':cityIn.val(),
+          'address':addressIn.val().toLowerCase(),
+          'address2':address2In.val().toLowerCase(),
+          'city':cityIn.val().toLowerCase(),
+          'state':stateIn.val().toLowerCase(),
           'zip':zipIn.val(),
           'date':dateIn.val(),
           'time':timeIn.val()
         };
+    console.log(data);
     socket.emit('registration',data);
     console.log('Registration Submitted');      
   });
-//SOCKET INTERACTIONS
-  //how client deals with Response Data
+  //Server confirmation that database registration took place
   socket.on('registration-confirmation', function(rows) {
     console.log("Registration Recieved!");
+    conf.css('display','block');
   });
+
+  //When Police Address Form submission is clicked:
+  // p for police
+  var pAddress = $(document.getElementById('paddress')),
+      pAddress2 = $(document.getElementById('paddress2')),
+      pCity = $(document.getElementById('pcity')),
+      pState = $(document.getElementById('pstate')),
+      pZip = $(document.getElementById('pzipcode')),
+    
+      pSubmit = $(document.getElementById('psubmit')),
+
+      returnName = $(document.getElementById('returnName')),
+      returnAddress = $(document.getElementById('returnAddress'));
+
+  pSubmit.on('click', function() {
+    console.log("Police Submit Button Clicked!");
+    var data = {
+          'address':pAddress.val().toLowerCase(),
+          'address2':pAddress2.val().toLowerCase(),
+          'city':pCity.val().toLowerCase(),
+          'state':pState.val().toLowerCase(),
+          'zip':pZip.val()
+        };
+    console.log(data);
+    socket.emit('police-query', data);
+    console.log("Police Query Submitted");
+  });
+  socket.on('police-response', function(data) {
+    console.log(data);
+    for(int i=0; i<data.length; i++) {
+      
+    }
+
+  });
+
+//SOCKET INTERACTIONS
+  //how client deals with Response Data
+  //informer for server-side / sql errors
+  socket.on('info', function(err) {
+    console.log(err);
+  });
+
 });
 
--- server.js
+++ server.js
@@ -18,11 +18,27 @@ console.log('Database Connection Established!');
 
 io.on('connection', function(socket) {
   console.log('Connection to Client Established!');
+  var date = new Date().getTime(),
+      check,
+      sql = "SELECT party_id, date FROM Parties";
+  connection.query(sql, function(err, rows, fields) {
+    if(err) console.log(err);
+    else {
+      for(var o in rows) {
+        check = new Date(rows[o].date.toString());
+        if(check < date) {
+          console.log(rows[o].party_id)
+          connection.query("DELETE FROM Parties WHERE party_id=" + rows[o].party_id);
+        }
+      }
+    }
+  });
 
   //when the display button is clicked, this request is sent to retreive current database information.
   socket.on('registration', function(data) { //{first_name,last_name,student_id,phone,address,date,time}
     console.log('Registration request recieved!');
-    var sql = "INSERT INTO parties (student_id, first_name, last_name, phone, phone2, address, address2, city, zipcode, date, time) VALUES(";
+    console.log(data.date);
+    var sql = "INSERT INTO Parties (student_id, first_name, last_name, phone, phone2, address, address2, city, state, zipcode, date, time) VALUES(";
     sql += "\"" +data.student_id+ "\",";
     sql += "\"" +data.first_name+ "\",";
     sql += "\"" +data.last_name+ "\",";
@@ -32,33 +48,65 @@ io.on('connection', function(socket) {
     sql += "\"" +data.address2+ "\",";
     sql += "\"" +data.city+ "\",";
     sql += "\"" +data.zip+ "\",";
+    sql += "\"" +data.state+ "\",";
     sql += "\"" +data.date+ "\",";
     sql += "\"" +data.time+ "\");";
-    connection.query(req.sql, function(err,rows,fields) {
-      if(err) io.emit('info', err);
+    connection.query(sql, function(err,rows,fields) {
+      if(err) { io.emit('info', err);console.log(err); }
       else {
-        io.emit('resgistration-confirmation', rows);
+        io.emit('registration-confirmation', rows);
         console.log("Registration Response Recieved By Server");
       }
     });
   });
 
   //when a new post is submitted, the sql is sent to the database through here and errors are returned.
-  socket.on('new-postData', function(req) {
-    console.log('New Post Submission Received!: '+req.sql);
-    if(req.sql!=="") {
-    connection.query(req.sql, function(err,rows,fields) {
-        if(err) io.emit('info', err);
-        else {
-          console.log("New Post Added to Database!");
-        }
-      });
+  socket.on('police-query', function(data) { //{address,address2,city,state,zip}
+    console.log('New Police Query Received!');
+
+    //Returns all rows associated with that address.
+    var sql = "SELECT first_name, phone FROM Parties WHERE ",
+        result;
+    
+    //Address Line 1
+    if(data.address!="") {
+        sql += "address LIKE \'%" + data.address + "%\'";
+        sql += " OR ";
     }
-    else console.log("No new post added to Database.");
+    //Address Line 2
+    if(data.address2!="") {
+        sql += "address2 LIKE \'%" + data.address2 + "%\'";
+        sql += " OR ";
+    }
+    //City Line
+    if(data.city!="") {
+        sql += "city LIKE \'%" + data.city + "%\'";
+        sql += " OR "; 
+    }
+    //State Line
+    if(data.state!="") {
+        sql += "state LIKE \'%" + data.state + "%\'";
+        sql += " OR ";
+    }
+    //Zipcode line
+    if(data.zip!="") {
+        sql += "zipcode LIKE \'%" + data.zip + "%\'";
+        sql += " OR ";
+    }
+    sql = sql.substring(0,sql.length-4) + ';';
+    console.log(sql);
+    //Query Database to return party info
+    connection.query(sql, function(err,rows,fields) {
+      if(err) console.log(err);
+      else {  
+        result = { 'rows':rows };
+        console.log(result);
+        io.emit('police-response', result);
+      }
+    });
+    console.log("Police Response Sent!"); 
   });
 });
-
-
 //send page to address on req. to default directory
   app.use(express.static(__dirname + '/public'));
 
